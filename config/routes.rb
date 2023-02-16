3b27d1db30f0456660d0f8f841173e1c474717a8
AshwinCarvalho999 <ashwincarvalho27@gmail.com>
2023-02-16 18:32:22 +0530

    initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :articles
   devise_for :users
   root 'splash_screen#index'
   resources :users, only: %i[ create update destroy]
